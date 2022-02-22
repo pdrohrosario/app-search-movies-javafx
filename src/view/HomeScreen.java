9aabaf884e9595254fcc324b1abb863919f605c4
Pedro Rosário <pedro.rosario@neomind.com.br>
2022-02-21 23:39:55 -0300

    Tratamento para busca no HomeScreen.class

--- src/view/HomeScreen.java
+++ src/view/HomeScreen.java
@@ -1,6 +1,9 @@
 package view;
 
 import java.awt.*;
+import java.util.Collection;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import utils.MovieUtils;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,18 +15,22 @@ public class HomeScreen extends JFrame implements ActionListener
 	JTextField searchBar = new JTextField();
 	JButton searchButton = new JButton("Search");
 	JButton clearButton = new JButton("Clean");
+	JLabel warning = new JLabel("");
 	private String lastMovie;
+	Alert alert;
 
-	public HomeScreen(){
+	public HomeScreen()
+	{
 		setTitle("IMovie - Encontre o filme que você gosta.");
-		setSize(600,600);
+		setSize(600, 600);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		initializeLayout();
 	}
 
-	public HomeScreen(String lastMovie){
+	public HomeScreen(String lastMovie)
+	{
 		setTitle("IMovie - Encontre o filme que você gosta.");
-		setSize(800,600);
+		setSize(800, 600);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		initializeLayout();
@@ -40,18 +47,22 @@ public class HomeScreen extends JFrame implements ActionListener
 		jp.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 
 		searchBar.setAlignmentX(LEFT_ALIGNMENT);
-		searchBar.setMaximumSize(new Dimension(700,45));
+		searchBar.setMaximumSize(new Dimension(700, 105));
 
 		searchButton.setAlignmentX(LEFT_ALIGNMENT);
-		searchButton.setSize(new Dimension(50,50));
+		searchButton.setSize(new Dimension(50, 50));
 
 		clearButton.setAlignmentX(LEFT_ALIGNMENT);
-		clearButton.setSize(new Dimension(50,50));
+		clearButton.setSize(new Dimension(50, 50));
+
+
+		warning.setVisible(false);
 
 		jp.add(searchButton);
 		jp.add(clearButton);
 
 		add(searchBar);
+		add(warning);
 		add(jp);
 
 		searchButton.setActionCommand("Search");
@@ -67,16 +78,27 @@ public class HomeScreen extends JFrame implements ActionListener
 		{
 			if (e.getActionCommand().equalsIgnoreCase("Search"))
 			{
-				setVisible(false);
-				new MovieScreen(MovieUtils.convertMovieTitle(searchBar.getText()));
+				if (!searchBar.getText().isEmpty())
+				{
+					setVisible(false);
+					warning.setVisible(false);
+					new MovieScreen(MovieUtils.convertMovieTitle(searchBar.getText()));
+				}
+				else
+				{
+					warning.setText("Digite o nome de um filme para fazer a busca !!");
+					warning.setVisible(true);
+				}
 			}
 			else if (e.getActionCommand().equalsIgnoreCase("Clear"))
 			{
 				searchBar.setText("");
+				warning.setVisible(false);
 				searchBar.requestFocus();
 			}
 		}
-		catch (Exception exc){
+		catch (Exception exc)
+		{
 			exc.printStackTrace();
 		}
 	}
